3b1d994afcf425d2634cf065266529323211550b
kalpanamodi2002 <55526279+kalpanamodi2002@users.noreply.github.com>
2022-03-30 18:32:32 +0530

    update usernotification_tb_strcuture

--- userNotification_db_structure.sql
+++ userNotification_db_structure.sql
@@ -1,5 +1,5 @@
 USE RMS;
-CREATE TABLE userNotification_tb_structure(
+CREATE TABLE UserNotification(
   userId INT primary key,
   notificationStatus BIT CHECK (notificationStatus IN(1,0)),
   foreign key(userId) REFERENCES userInfo_tb_structure(userId)
