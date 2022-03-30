352c5793a32bfce8989f9084954140f63409a9ad
Kalpana Modi <kalpanamodi2002@gmail.com>
2022-03-30 18:28:36 +0530

    Add MyDetails table structure

---
+++ MyDetails_tb_structure.sql
@@ -0,0 +1,7 @@
+use RMS;
+create table MyDetails(
+id int identity(1,1) primary key,
+resumeId int not null foreign key(resumeId) references Resume(resumeId),
+profilePhoto varchar(100) ,
+totalExp float
+);
